7f59f13a17697a1d83db03c9b46a7ecfe4d9e77a
plasma147 <plasma147@hotmail.com>
2014-09-27 00:15:27 +0100

    tidying up

--- src/blog_scrape/core.clj
+++ src/blog_scrape/core.clj
@@ -3,20 +3,11 @@
    (:require [net.cgrand.enlive-html :as html]
              [clojure.string :only (join)]))
   
-  (defn- fetch-page [page]
-    (html/html-resource (java.io.StringReader. (slurp page))))
-  
   (defn- extract-content [post]
     "Return a map containing title and a seq of the paragraphs as text"
     (let [title       (html/text (first (html/select [post] [:h2])))
           content (map html/text        (html/select [post] [:p]))]
       {:title title, :content content}))
-  
-  (defn- wrap-as-doc [content]
-    (str
-      "<html><head><meta charset=\"UTF-8\"></head><body>"
-      content
-      "</body></html>"))
   
   (defn- convert-to-html [post]
     (str
@@ -25,10 +16,18 @@
       "</h1>"
       (clojure.string/join (map #(str "<p>" % "</p>") (:content post)))))
 
+  (defn- wrap-as-doc [content]
+    (str
+      "<html><head><meta charset=\"UTF-8\"></head><body>"
+      content
+      "</body></html>"))
+
   (def posts 
     (->
       "resources/blog.html"
-      (fetch-page)
+      (slurp)
+      (java.io.StringReader.)
+      (html/html-resource)
       (html/select [:div.post])))
   
   (defn -main [& args]
